b1dffc5a7c7a6c76863f2f738c6fa49f7f6ccb4f
MaxCarman <1030847@student.sae.edu.au>
2025-02-25 22:22:11 +1000

    Improved dash physics and controls, transfering velocity on collision, and seperation of GameManager and PlayerController.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //Declare Variables
+    public float statMovespeed;
+    public float statJumpForce;
+    public float statDashForce;
+    public float staminaMaximum;
+    public float staminaIncrement;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,10 +22,18 @@ public class PlayerController : MonoBehaviour
 
     public bool inContactWithEnviroment; //If the player is in contact with a enviromental object.
 
-    //Energy
-    public float energyCurrent;   //Player's current energy. Decimals is progress to next energy refill.
-    public int energyMaximum;     //Players maximum potential energy.
-    public float energyIncrement; //The cooldown rate of energy. Increased by this amount eahc fixed tick.
+
+    //Player Stats
+    [SerializeField] private GameManager gameManager; //Reference to the game manager script, which unifies stats between players.
+
+    private float statMoveSpeed;
+    private float statJumpForce;
+    private float statDashForce;
+    private float staminaMaximum;     //Players maximum potential stamina.
+    private float staminaIncrement; //The cooldown rate of stamina. Increased by this amount each fixed tick.
+
+    //Stamina
+    public float staminaCurrent;   //Player's current stamina. Decimals is progress to next stamina refill.
 
     //Double Tap Detection
     [SerializeField] private float dtTime; //The length of fixed ticks between taps to register.
@@ -36,17 +44,13 @@ public class PlayerController : MonoBehaviour
     private int dtRight = 0;
     private int dtRightTime = 0;
 
-    //Player Identification
-    public int playerID;
+    private int dtDown = 0;
+    private int dtDownTime = 0;
 
-    //Player Colors
+    //Player Specifics
+    public int playerID;
     public Color ColorNonPhysics;
     public Color ColorYesPhysics;
-
-    //Player Stats
-    public float statMoveSpeed;
-    public float statJumpForce;
-    public float statDashForce;
 
     //Component References
     [SerializeField] private Rigidbody2D thisRigidbody2D;
@@ -58,65 +62,94 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        staminaMaximum = gameManager.staminaMaximum;
+        staminaCurrent = staminaMaximum;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Apply stats from game manager. This is in update because it changes over time.
+        statMoveSpeed = gameManager.statMovespeed;
+        statJumpForce = gameManager.statJumpForce;
+        statDashForce = gameManager.statDashForce;
+        staminaMaximum = gameManager.staminaMaximum;
+        staminaIncrement = gameManager.staminaIncrement;
+
         //Jumping (Dosent work in fixed update)
-        if (Input.GetKeyDown(controlUp) && energyCurrent >= 1) //Check for energy as well
+        if (Input.GetKeyDown(controlUp) && staminaCurrent >= 1) //Check for stamina as well
         {
             if (InPhysicsMode == false) //If not in physic mode, jump normally
             {
-                energyCurrent -= 1;
+                staminaCurrent -= 1;
                 thisRigidbody2D.velocity = new Vector2(thisRigidbody2D.velocity.x, statJumpForce);
             }
 
             if (InPhysicsMode == true && PhysicsFrames == 0) //Extra checks are required for escaping physics mode via jumping
             {
-                energyCurrent -= 1;
+                staminaCurrent -= 1;
                 InPhysicsMode = false;
                 thisRigidbody2D.velocity = new Vector2(thisRigidbody2D.velocity.x, statJumpForce);
             }
         }
 
         //Dashing Detection
-        if(InPhysicsMode == false)
+        //Dashing Detection
+        if (InPhysicsMode == false || InPhysicsMode == true) //Currently supports both modes, but may change so i kept this condition in.
         {
             //Left Dashing
-            if(Input.GetKeyDown(controlLeft) && energyCurrent >= 1)
+            if (Input.GetKeyDown(controlLeft) && staminaCurrent >= 1)
             {
                 dtLeft += 1;
             }
-            if(dtLeft >= 2)
+            if (dtLeft >= 2)
             {
-                energyCurrent -= 1;
+                //Reset taps and apply motion/stamina changes.
+                dtLeft = 0;
+                staminaCurrent -= 1;
                 InPhysicsMode = true;
-                thisRigidbody2D.AddForce(new Vector2(-statDashForce, 0));
+                thisRigidbody2D.velocity += new Vector2(-statDashForce, 0);
             }
 
             //Right Dashing
-            if (Input.GetKeyDown(controlRight) && energyCurrent >= 1)
+            if (Input.GetKeyDown(controlRight) && staminaCurrent >= 1)
             {
                 dtRight += 1;
             }
             if (dtRight >= 2)
             {
-                energyCurrent -= 1;
+                //Reset taps and apply motion/stamina changes.
+                dtRight = 0;
+                staminaCurrent -= 1;
                 InPhysicsMode = true;
-                thisRigidbody2D.AddForce(new Vector2(statDashForce, 0));
+                thisRigidbody2D.velocity += new Vector2(statDashForce, 0);
+            }
+
+            //Down Dashing
+            if (Input.GetKeyDown(controlDown) && staminaCurrent >= 1)
+            {
+                dtDown += 1;
+            }
+            if (dtDown >= 2)
+            {
+                //Reset taps and apply motion/stamina changes.
+                dtDown = 0;
+                staminaCurrent -= 1;
+                InPhysicsMode = true;
+                thisRigidbody2D.velocity += new Vector2(0, -statDashForce);
             }
         }
 
         //Update Physics Material and colours based on physics mode.
-        if(InPhysicsMode == false)
+        if (InPhysicsMode == false)
         {
             thisRigidbody2D.sharedMaterial = NonPhysicsMaterial;
+            thisCircleCollider2D.sharedMaterial = NonPhysicsMaterial;
             gameObject.GetComponent<Renderer>().material.color = ColorNonPhysics;
         } else
         {
             thisRigidbody2D.sharedMaterial = YesPhysicsMaterial;
+            thisCircleCollider2D.sharedMaterial = YesPhysicsMaterial;
             gameObject.GetComponent<Renderer>().material.color = ColorYesPhysics;
         }
 
@@ -125,7 +158,7 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         //Non-Physics Movement
-        if(InPhysicsMode == false) //Check if the player should be capable of movement
+        if (InPhysicsMode == false) //Check if the player should be capable of movement
         {
 
             //Left Right movement
@@ -148,24 +181,28 @@ public class PlayerController : MonoBehaviour
                 thisRigidbody2D.velocity = new Vector2(0, thisRigidbody2D.velocity.y);
             }
         }
-        
+
         //If the player is motionless or close to motionless in physics mode, set back to non-physics mode.
-        if(InPhysicsMode == true && (thisRigidbody2D.velocity.x < 1 && thisRigidbody2D.velocity.x > -1) && (thisRigidbody2D.velocity.y < 1 && thisRigidbody2D.velocity.y > -1))
+        if (InPhysicsMode == true && (thisRigidbody2D.velocity.x < 1.5 && thisRigidbody2D.velocity.x > -1.5) && (thisRigidbody2D.velocity.y < 1.5 && thisRigidbody2D.velocity.y > -1.5))
         {
             InPhysicsMode = false;
         }
 
-        //Update double tap timers
-        if(dtLeft >= 1)
+        //Dashing - Update dashing timers
+        //Left Dash
+        if (dtLeft >= 1)
         {
             dtLeftTime += 1;
-            if(dtLeftTime >= dtTime)
+            if (dtLeftTime >= dtTime)
             {
                 dtLeft = 0;
                 dtLeftTime = 0;
             }
+        } else
+        {
+            dtLeftTime = 0;
         }
-
+        //Right Dash
         if (dtRight >= 1)
         {
             dtRightTime += 1;
@@ -174,18 +211,34 @@ public class PlayerController : MonoBehaviour
                 dtRight = 0;
                 dtRightTime = 0;
             }
+        } else
+        {
+            dtRightTime = 0;
+        }
+        //Down Dash
+        if (dtDown >= 1)
+        {
+            dtDownTime += 1;
+            if (dtDownTime >= dtTime)
+            {
+                dtDown = 0;
+                dtDownTime = 0;
+            }
+        } else
+        {
+            dtDownTime = 0;
         }
 
-        //Restore Stamina
-        if(inContactWithEnviroment == true)
+        //Restore Stamina if in contact with the enviroment
+        if (inContactWithEnviroment == true)
         {
-            if(energyCurrent < energyMaximum)
+            if(staminaCurrent < staminaMaximum)
             {
-                energyCurrent += energyIncrement; //If below, add the increment.
+                staminaCurrent += staminaIncrement; //If below, add the increment.
             }
-            if(energyCurrent > energyMaximum)
+            if(staminaCurrent > staminaMaximum)
             {
-                energyCurrent = energyMaximum; //If above, lower to make it exact.
+                staminaCurrent = staminaMaximum; //If above, lower to make it exact.
             }
         }
 
@@ -195,6 +248,7 @@ public class PlayerController : MonoBehaviour
     //Collison Detection
     void OnCollisionStay2D(Collision2D col)
     {
+        //If colliding with the enviroment, set to true.
         if (col.gameObject.CompareTag("Enviroment"))
         {
             inContactWithEnviroment = true;
@@ -203,9 +257,25 @@ public class PlayerController : MonoBehaviour
 
     void OnCollisionExit2D(Collision2D col)
     {
+        //When leaving collision with the enviroment, set to false.
         if (col.gameObject.CompareTag("Enviroment"))
         {
             inContactWithEnviroment = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        //Apply velocity to this object is another high velocity object hits it.
+        if(col.gameObject.GetComponent<Rigidbody2D>() != null && InPhysicsMode == false) //Check if null and in non-physics mode.
+        {
+            Rigidbody2D colRigidbody2D = col.gameObject.GetComponent<Rigidbody2D>();
+            if ((colRigidbody2D.velocity.x > 1.5f || colRigidbody2D.velocity.x < -1.5f) || (colRigidbody2D.velocity.y > 1.5f || colRigidbody2D.velocity.y < -1.5f)) //Check if rigidbody in motion.
+            {
+                Debug.Log("apply velocity");
+                InPhysicsMode = true;
+                thisRigidbody2D.velocity += colRigidbody2D.velocity;
+            }
         }
     }
 }
